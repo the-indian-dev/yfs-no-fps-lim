a0df9d9a55f6e49d3e55ed6f5f037fd8f02f079a
Ritabrata Das <ritabrata.das2669@gmail.com>
2025-05-25 18:35:44 +0530

    Add build scripts

---
+++ build-release.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/fish
+
+rm -rf build/
+mkdir build
+cd build
+cmake ../src -DCMAKE_C_FLAGS="-Wno-incompatible-pointer-types" -DCMAKE_CXX_FLAGS="-Wno-incompatible-pointer-types" -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+
+cmake --build . --config Release --parallel
